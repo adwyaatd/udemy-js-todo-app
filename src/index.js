d6cdb8f943dc8c33714907124b1a7b865aca5935
細田 来夢 <52363465+adwyaatd@users.noreply.github.com>
2021-09-27 07:56:05 +0900

    5-34

--- src/index.js
+++ src/index.js
@@ -24,7 +24,9 @@ const onClickAdd = () => {
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
-    alert("削除");
+    //押された削除ボタンの親divタグを未完了リストから削除
+    const deleteTarget = deleteButton.parentNode;
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
   });
 
   // divの子要素に各要素を配置
